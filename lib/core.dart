21b515391846825ddd096cf70e07f2efa0d312ed
纽特 <coorchice.cb@alibaba-inc.com>
2019-03-14 18:27:14 +0800

    update

---
+++ lib/core.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/services.dart';
+
+const platformMethodChannel = const MethodChannel('com.coorchice.flutter_app');
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 import 'pages/splash_page.dart';
 
-void main() => runApp(MaterialApp(
-  title: 'Flutter',
-  home: SplashPage(),
-));
+void main() => runApp(_widgetForRoute(window.defaultRouteName));
 
-
+Widget _widgetForRoute(String route) {
+  switch (route) {
+    case 'route0':
+      return MaterialApp(
+        title: 'Flutter',
+        home: SplashPage(),
+      );
+    case 'route1':
+    case 'route2':
+    case 'route3':
+    default:
+      return Center(
+        child: Text('Unknown route: $route', textDirection: TextDirection.ltr),
+      );
+  }
+}
